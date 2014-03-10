2b8864f4aa7114e492b564616111e1218ad2f46b
Alex Jacobs <lex.jacobs@gmail.com>
2014-03-09 17:38:46 -0700

    n-rooks solved

--- src/solvers.js
+++ src/solvers.js
@@ -62,7 +62,7 @@ var naiveBoardSetGenerator = function(n){
     }
     finalBoardArray.push(singleBoard);
   }
-  console.log(finalBoardArray);
+  // console.log(finalBoardArray);
   return finalBoardArray;
 };
 
@@ -71,74 +71,22 @@ var naiveBoardSetGenerator = function(n){
 // this is a set of boards that can be iterated over. the solution for
 // rooks is self-contained. for queens, check for diagonal conflicts.
 
+// generate all rook solutions for n 1-8:
 
-
-
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-// [[0,1,0],
-// [0,0,1],
-// [1,0,0],
-// ]
-
-// generate all solutions for n = 3
-
-
-
-
-
-var n3maker = function(){
-  var i;
-  var boardCount = 0;
-  var results = {};
-  for(i = 0; i < 3; i++){
-    for(var j =0; j < 3; j++){
-      for(var k = 0; k < 3; k++){
-        var start = [];
-        start.push([],[],[]);
-        for(var a = 0; a < start.length; a++){
-          start[a].push(0,0,0);
-        }
-        var subResults = start.slice();
-        subResults[0][i] = 1;
-        subResults[1][j] = 1;
-        subResults[2][k] = 1;
-        boardCount++;
-        results[boardCount] = subResults.slice();
-      }
-    }
-  }
-  //console.log(results);
-  return results;
+var rookSolver = function(n){
+  var nSolutionSet = naiveBoardSetGenerator(n);
+  return nSolutionSet;
 };
 
- var boardSet = n3maker();
-
- for( var n in boardSet){
-  var thisBoard = new Board(boardSet[n]);
-     if(thisBoard.hasAnyColConflicts()){
-      delete boardSet[n];
-     }
- }
-
- console.log(boardSet);
-
-//
-
-
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-
+  var solution = rookSolver(n)[0];
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
-
-
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = rookSolver(n).length;
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
